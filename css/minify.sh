933031207060e8ad97c8b2825109b624038c39bf
Azema <manuel.hervo@gmail.com>
2021-11-07 10:14:14 +0100

    Ajout de la classe seen

--- css/minify.sh
+++ css/minify.sh
@@ -14,10 +14,14 @@ file=$(basename -- "$1")
 filename="${file%.*}"
 extension="${file##*.}"
 filenameMin="$filename.min.$extension"
+compressor="/usr/bin/yui-compressor"
+if [ ! -f "$compressor" ]; then
+  compressor="./yui-compressor";
+fi
 
 echo "file: $file, filename: $filename, extension: $extension, filename min: $filenameMin"
 
-yui-compressor --type css --charset utf-8 -v -o "$filenameMin" "$file"
+$compressor --type css --charset utf-8 -v -o "$filenameMin" "$file"
 
 integrity=`cat $filenameMin | openssl dgst -sha384 -binary | openssl enc -base64 -A`
 echo "Integrity sha384-$integrity"
