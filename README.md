920aa70728df348ed90492c74a7b19e693e5b2c1
Azema <manuel.hervo@gmail.com>
2021-12-26 05:57:40 +0100

    Update bundle.js

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ Repository servant pour l'authentification OAuth avec l'API de BetaSeries, ainsi
 
 ### JS
 * renderjson.min.js: `sha384-ISyV9OQhfEYzpNqudVhD/IgzIRu75gnAc0wA/AbxJn+vP28z4ym6R7hKZXyqcm6D`
-* app-bundle.js: `sha384-s2wev+sMX+1HOM4Ly7HWn0/VAlvxVvZK8rWreW3YOctKDi6FzNMsoSQQnP9lwfgX`
+* app-bundle.js: `sha384-J3hlglB4YVqbxWWr50yIxHDK8EOgOwqugSA3YgAtnyNfEqUhRklii8lXB7pGvSOC`
--- js/app-bundle.js
+++ js/app-bundle.js
@@ -1735,7 +1735,7 @@ class Base {
                      style="margin-top: 2px; min-height: 0">
                     <button type="button" class="btn-reset btnSubscribe" style="position: absolute; top: 3px; right: 31px; padding: 8px;">
                         <span class="svgContainer">
-                            <svg fill="rgba(255, 255, 255, .5)" width="14" height="16" style="position: relative; top: 1px; left: -1px;">
+                            <svg fill="${Base.theme == 'dark' ? 'rgba(255, 255, 255, .5)' : '#333'}" width="14" height="16" style="position: relative; top: 1px; left: -1px;">
                                 <path fill-rule="nonzero" d="M13.176 13.284L3.162 2.987 1.046.812 0 1.854l2.306 2.298v.008c-.428.812-.659 1.772-.659 2.806v4.103L0 12.709v.821h11.307l1.647 1.641L14 14.13l-.824-.845zM6.588 16c.914 0 1.647-.73 1.647-1.641H4.941c0 .91.733 1.641 1.647 1.641zm4.941-6.006v-3.02c0-2.527-1.35-4.627-3.705-5.185V1.23C7.824.55 7.272 0 6.588 0c-.683 0-1.235.55-1.235 1.23v.559c-.124.024-.239.065-.346.098a2.994 2.994 0 0 0-.247.09h-.008c-.008 0-.008 0-.017.009-.19.073-.379.164-.56.254 0 0-.008 0-.008.008l7.362 7.746z"></path>
                             </svg>
                         </span>
