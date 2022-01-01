b5f6ab861a870fe90bd9825ed17ae7833d787f8a
Azema <manuel.hervo@gmail.com>
2022-01-01 21:18:24 +0100

    Update bundle.js

--- README.md
+++ README.md
@@ -11,4 +11,4 @@ Repository servant pour l'authentification OAuth avec l'API de BetaSeries, ainsi
 
 ### JS
 * renderjson.min.js: `sha384-ISyV9OQhfEYzpNqudVhD/IgzIRu75gnAc0wA/AbxJn+vP28z4ym6R7hKZXyqcm6D`
-* app-bundle.js: `sha384-a94T2D9Yf4Ge3BAfI964t5PO0FyELM+9G7Pj3mSUVCOWifydxoRqXPPN/rtNRF8s`
+* app-bundle.js: `sha384-H4SDPWoQ6ZW3q2qC3LUM252VxqkcmlTD8xTkoga6V5RpktaRnexZxmGM/ZyH/u7k`
--- js/app-bundle.js
+++ js/app-bundle.js
@@ -1446,7 +1446,7 @@ class CommentBS {
             else {
                 // On masque
                 $replies.fadeOut('fast');
-                $btn.find('.btnText').text(Base.trans("comment.button.reply", { "%count%": $replies.length.toString() }));
+                $btn.find('.btnText').text(Base.trans("comment.button.reply", { "%count%": $replies.length.toString() }, $replies.length));
                 $btn.find('svg').attr('style', 'transition: transform 200ms ease 0s;');
                 $btn.data('toggle', '0');
             }
